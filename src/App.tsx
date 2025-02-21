6ebc1e96f418f3052b365c76898c6b5cdc198bc7
The Real <81657406+realdyl@users.noreply.github.com>
2025-02-21 04:19:58 -0600

    v1.9
    Major UI sync

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import DeepSeekChat from './components/DeepSeekChat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BLXCK.chat</h1>
+      </header>
+      <main>
+        <DeepSeekChat />
+      </main>
+    </div>
+  );
+}
+
+export default App;
